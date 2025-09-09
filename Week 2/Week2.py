2a9b245c0acd72e34307c6e40f9ef367fa77f6f0
Edwin31430 <564099@student.fontys.nl>
2025-09-09 16:20:08 +0200

    space code

--- Week 2/Week2.py
+++ Week 2/Week2.py
@@ -4,10 +4,17 @@ with open(file_path, 'r') as file:
 csv_reader = csv.DictReader(file)
 for row in csv_reader:
 records.append(row)
+
+
+
 total = sum(float(record['Grade']) for record in records)
 average = total / len(records)
 print(f"Average Grade: {average}")
 print("--------------------")
+
+
+
+
 filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
 print("Student Report")
 print("--------------")
