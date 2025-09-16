a0265feb7245f9907f91195fd6ff8bc92d748fac
Edwin31430 <564099@student.fontys.nl>
2025-09-16 17:30:03 +0200

    func24

--- Week 2/Exercise2.py
+++ Week 2/Exercise2.py
@@ -1,1 +1,48 @@
 
+
+
+
+
+
+
+
+
+
+
+#2
+def pixel_to_complex(pixel_x: int, pixel_y: int, width: int, x_range: tuple[float, float], y_range: tuple[float, float]):
+    "Convert pixel coordinates (x, y) into a complex number c = x + yi within the given ranges"
+
+    x_min, x_max = x_range
+    y_min, y_max = y_range
+
+    x = x_min + (pixel_x / (width - 1)) * (x_max - x_min)
+    y = y_min + (pixel_y / (width - 1)) * (y_max - y_min)
+
+    return complex(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#4
+def draw_mandel(width: int, max_integer: int = 100):
+    "Display a Mandelbrot image"
+    
+    from __main__ import generate_mandelbrot, plot_mandelbrot  
+
+    data = generate_mandelbrot(width, max_integer=max_integer)
+    plot_mandelbrot(data)
+
+
+
