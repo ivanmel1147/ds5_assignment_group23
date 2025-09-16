3c901744b987c0ca9153ae0a68d38abd76b26582
Ivan <i.melnychuk@student.fontys.nl>
2025-09-16 10:01:33 +0200

    Started exercise 1

--- Week 2/Week2.py
+++ Week 2/Week2.py
@@ -1,3 +1,4 @@
+import csv
 file_path = input("Enter the path to the CSV file: ")
 records = []
 with open(file_path, 'r') as file:
@@ -20,3 +21,8 @@ def filter_record(records):
     filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
     print("Student Report")
     print("--------------")
+
+for record in filtered_records:
+    print(f"Name: {record['Name']}")
+    print(f"Grade: {record['Grade']}")
+    print("--------------------")
