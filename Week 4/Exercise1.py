9896b4c194f1444f30e183fde8ef33b350e1e0c8
Edwin31430 <564099@student.fontys.nl>
2025-09-27 14:57:36 +0200

    Update Exercise1.py
    Cleaning Data

--- Week 4/Exercise1.py
+++ Week 4/Exercise1.py
@@ -10,3 +10,10 @@ df.head()
 # 1. Duplicate rows
 # 2. Missing values
 
+# 1. Handling duplicate rows
+data1_clean = df.drop_duplicates()
+data1_clean
+
+# 2. Handling missing values
+data2_clean = data1_clean.dropna()
+data2_clean
