c3129eb835effe2333ccfa3a06222ec8084cd423
Edwin31430 <564099@student.fontys.nl>
2025-10-04 14:26:15 +0200

    Revert "Added function to load dataset"
    This reverts commit 92c16845f5f0a0004a6248c765625fbf569d7769.

--- Week 4/Exercise2.py
+++ Week 4/Exercise2.py
@@ -2,21 +2,6 @@ import pandas as pd
 import numpy as np
 
 # Load the dataset
-# df = pd.read_excel('detailedRetail.xlsx')
-# print(df.shape)
-# df.head()
-
-def load_data(file_path: str) -> pd.DataFrame:
-    """
-    Load sales data from an Excel file.
-
-    Parameters:
-        file_path (str): Path to the Excel file.
-
-    Returns:
-        pd.DataFrame: The loaded sales dataset.
-
-    Author:
-        John Doe
-    """
-    return pd.read_excel(file_path)
+df = pd.read_csv('detailedRetail.xlsx')
+print(df.shape)
+df.head()
