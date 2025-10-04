f43ad4665a84c9ad5a46b386ea919fe0b8dace00
Edwin31430 <564099@student.fontys.nl>
2025-10-04 13:59:10 +0200

    Add Corrected Files

--- Week 4/Exercise1.py
+++ Week 4/Exercise1.py
@@ -2,18 +2,18 @@ import pandas as pd
 import numpy as np
 
 # Load the dataset
-df = pd.read_csv('hotelBookings.xlsx')
-print(df.shape)
-df.head()
+#df = pd.read_excel('hotelBookings.xlsx')
+#print(df.shape)
+#df.head()
 
 # Mistakes in the dataset
 # 1. Duplicate rows
 # 2. Missing values
 
 # 1. Handling duplicate rows
-data1_clean = df.drop_duplicates()
-data1_clean
+# data1_clean = df.drop_duplicates()
+# data1_clean
 
 # 2. Handling missing values
-data2_clean = data1_clean.dropna()
-data2_clean
+# data2_clean = data1_clean.dropna()
+# data2_clean
