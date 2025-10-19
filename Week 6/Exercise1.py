021129aa9ef986d85e7e70a01128dd671a0babe0
Edwin31430 <564099@student.fontys.nl>
2025-10-19 17:53:54 +0200

    Copy code from exercise

---
+++ Week 6/Exercise1.py
@@ -0,0 +1,16 @@
+#a)
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(2)  
+
+x = np.random.uniform(0, 10, 200)
+y = 2 * x**2 - 5 * x + 3 + np.random.normal(0, 10, 200)
+
+# Plot the dataset
+plt.scatter(x, y)
+plt.xlabel('x')
+plt.ylabel('y') 
+plt.title('Dataset') 
+plt.show()   
+
