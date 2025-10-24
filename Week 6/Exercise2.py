5fe1eb372d6057b52c67c723c461963261471e93
Edwin31430 <564099@student.fontys.nl>
2025-10-24 17:35:33 +0200

    Visualize Distribution

--- Week 6/Exercise2.py
+++ Week 6/Exercise2.py
@@ -13,3 +13,16 @@ print(df.info())
 
 #c)
 df = df.dropna()
+
+#d)
+df['quality'].hist()
+plt.title('Distribution of Wine Quality Ratings')
+plt.xlabel('Wine Quality')
+plt.ylabel('Frequency')
+plt.show()
+
+plt.scatter(df['alcohol'], df['quality'])
+plt.title('Quality vs Alcohol')
+plt.xlabel('Alcohol')
+plt.ylabel('Quality')
+plt.show()
